8000a893db3c80b6c3526bb7b5ef69b6f533a296
Mitay <dimonmai2008@yandex.ru>
2019-09-05 09:55:17 -0700

    Initial Commit

---
+++ Slider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slider
+//
+//  Created by mitay on 05/09/2019.
+//  Copyright © 2019 mitay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
